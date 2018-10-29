2ed7510a6866cfd1dba5d5a0982495c4c25b20fa
juho linden <juho.linden@gmail.com>
2018-10-29 17:14:55 +0200

    updatee

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -14,8 +14,54 @@ const favoriteBlog = (blogs) => {
   return mostliked
 }
 
+const mostBlogs = (blogs) => {
+  const kirjoittajat = blogs.map(x => x.author)
+  let blogaajat = {}
+  let counter = 0
+  let ahkerin ={ author: '', blogs: '' }
+  for(let i = 0; i < blogs.length; i++) {
+    if(blogaajat[kirjoittajat[i]] === undefined) {
+      blogaajat[kirjoittajat[i]] = 1
+    } else {
+      blogaajat[kirjoittajat[i]] = blogaajat[kirjoittajat[i]] +1
+    }
+
+    if(blogaajat[kirjoittajat[i]] > counter) {
+      counter = blogaajat[kirjoittajat[i]]
+      ahkerin = { author: kirjoittajat[i],
+        blogs: counter }
+    }
+  }
+  return ahkerin
+}
+
+const mostLikes = (blogs) => {
+  const kirjoittajat = blogs.map(x => {
+    return { author: x.author, likes: x.likes }})
+  let blogaajat = []
+  let counter = 0
+  let ahkerin ={ author: '', likes: '' }
+  for(let i = 0; i < blogs.length; i++) {
+    if(blogaajat.filter(x => x.author === kirjoittajat[i].author).length === 0) {
+      blogaajat = blogaajat.concat(kirjoittajat[i])
+    } else {
+      blogaajat[blogaajat.findIndex(x => x.author === kirjoittajat[i].author)] = { author: kirjoittajat[i].author,
+        likes: blogaajat[blogaajat.findIndex(x => x.author === kirjoittajat[i].author)].likes + kirjoittajat[i].likes }
+    }
+    if(blogaajat[blogaajat.findIndex(x => x.author === kirjoittajat[i].author)].likes > counter) {
+      counter = blogaajat[blogaajat.findIndex(x => x.author === kirjoittajat[i].author)].likes
+      ahkerin = { author: kirjoittajat[i].author,
+        likes: counter }
+    }
+  }
+  return ahkerin
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
+
 }
